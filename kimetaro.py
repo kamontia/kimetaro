bb4417b62cdb36aedf5b992fbf39166b297d6e55
kondo takeshi <chaspy@users.noreply.github.com>
2019-04-29 16:25:21 +0900

    Cache no such file exception

--- kimetaro.py
+++ kimetaro.py
@@ -77,8 +77,9 @@ def main():
     try:
         with open("./config.ini") as k:
             parser.read_file(k)
-    except:
-        pass
+    except FileNotFoundError as e:
+        print(e)
+        raise
 
     global ACCESSTOKEN, LIST, MAX_ITEM
     ACCESSTOKEN = parser["TOKEN"]["ACCESSTOKEN"]
